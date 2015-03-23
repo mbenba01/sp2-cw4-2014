3e42e5ca244db8aa069b592810477d7344dc3f9e
mbenba01 <mbenba01@mail.bbk.ac.uk>
2015-03-23 20:51:27 +0000

    Added ShipTest.java

--- battleship/src/battleship/ShipTest.java
+++ battleship/src/battleship/ShipTest.java
@@ -20,8 +20,32 @@ public class ShipTest {
 	
 	@Test
 	public void testSetHitArray() {
-		Ship submarine = new Submarine();
-		submarine.setHitArray();
+		Ship submarine = new Ship();
+		//submarine.setHitArray();
+	}
+	
+	@Test
+	public void testOkToPlaceShipAt() {
+		
+		Ocean board = new Ocean();
+		
+		Ship[][] ships = new Ship[10][10];
+		
+		board.print();
+		
+		boolean horizontal = false;
+		
+		int row = 0;
+		int column = 0;
+		
+		for(row = 0; row < ships.length; row++) {
+			for(column = 0; column < ships[row].length; column++) {
+				if(ships[row][column].okToPlaceShipAt(row, column, horizontal, board))
+					System.out.print(2);
+			}
+			System.out.println();
+		}
+		
 	}
 
 }
