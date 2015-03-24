e492655c7872ee3d684b798f8c365a449ceefabe
Mustapha Benbaziz <mbenba01@mail.bbk.ac.uk>
2015-03-24 16:16:29 +0000

    Moved ShipTest.java from battleship package to newly created unitTests package

---
+++ battleship/src/unitTests/ShipTest.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package unitTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import battleship.Ocean;
+import battleship.Ship;
+
+/**
+ * @author Mustapha Benbaziz
+ *
+ */
+public class ShipTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testSetHitArray() {
+		Ship submarine = new Ship();
+		//submarine.setHitArray();
+	}
+	
+	@Test
+	public void testOkToPlaceShipAt() {
+		
+	}
+
+}
