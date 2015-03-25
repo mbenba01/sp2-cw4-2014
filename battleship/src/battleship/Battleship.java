d516666f59d19cec69972760394de670bd9319fc
Mustapha Benbaziz <mbenba01@mail.bbk.ac.uk>
2015-03-25 09:51:58 +0000

    Added necessary methods and constructor to initialise the length of the battleship, and set the hit array to false

--- battleship/src/battleship/Battleship.java
+++ battleship/src/battleship/Battleship.java
@@ -1,22 +1,38 @@
 /**
- * 
+ * This class extends Ship and represents a vessel of type battleship 
+ * with the length of 4
  */
 package battleship;
 
 /**
- * @author mustaphamustapha
+ * @author Mustapha Benbaziz
  *
  */
 public class Battleship extends Ship {
 	
+	
+	/**
+	 * The constructor initialises the length of the battleship to 4
+	 * and the hit array to false
+	 */
 	public Battleship() {
+		
 		length = 4;
+		
+		hit = new boolean[this.length];
+		
+		for(int i = 0; i < hit.length; i++) {
+			hit[i] = false;
+		}
+		
 	}
+
 
 	@Override
 	public int getLength() {
 		return length;
 	}
+	
 	
 	@Override
 	public String getShipType() {
